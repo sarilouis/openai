2c8338e3816fc36d35fdbb4e38d174533200af48
Sari Louis <slouis@gmail.com>
2022-09-04 18:48:12 -0400

    Cleaned up unused using

--- OpenAI.Playground/TestHelpers/CompletionTestHelper.cs
+++ OpenAI.Playground/TestHelpers/CompletionTestHelper.cs
@@ -1,7 +1,6 @@
 ﻿using OpenAI.GPT3.Interfaces;
 using OpenAI.GPT3.ObjectModels;
 using OpenAI.GPT3.ObjectModels.RequestModels;
-using System.Threading.Tasks.Sources;
 
 namespace OpenAI.Playground.TestHelpers
 {
